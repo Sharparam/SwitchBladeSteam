21cc679002d54c21154fe54dc9bcff48b04283d7
F16Gaming <adam.hellberg95@gmail.com>
2013-01-27 20:08:58 +0100

    Version update.

--- F16Gaming.SwitchBladeSteam.App/Properties/AssemblyInfo.cs
+++ F16Gaming.SwitchBladeSteam.App/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.41.1157")]
-[assembly: AssemblyFileVersion("1.0.41.1157")]
+[assembly: AssemblyVersion("1.0.42.0635")]
+[assembly: AssemblyFileVersion("1.0.42.0635")]
