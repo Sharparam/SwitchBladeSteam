88cc09244775c8fbc2072ba4b11ab5b4b3f535d6
Adam Hellberg <sharparam@sharparam.com>
2013-11-21 01:43:24 +0100

    Removed commented code and debug statements

--- Sharparam.SwitchBladeSteam/App.xaml.cs
+++ Sharparam.SwitchBladeSteam/App.xaml.cs
@@ -50,14 +50,6 @@ namespace Sharparam.SwitchBladeSteam
                                                  @"Default\Images\dk_offline.png",
                                                  @"Default\Images\dk_offline_pressed.png", true);
 
-            //razer.EnableDynamicKey(RazerAPI.DynamicKeyType.DK2, (sender, args) =>
-            //    {
-            //        _onlineKey.Refresh();
-            //        _busyKey.Refresh();
-            //        _awayKey.Refresh();
-            //        _offlineKey.Refresh();
-            //    }, "null");
-
             try
             {
                 var steam = Provider.Steam;
@@ -112,20 +104,16 @@ namespace Sharparam.SwitchBladeSteam
                 case EPersonaState.k_EPersonaStateMax:
                 case EPersonaState.k_EPersonaStateLookingToPlay:
                 case EPersonaState.k_EPersonaStateLookingToTrade:
-                    Console.WriteLine("Resetting online key");
                     _onlineKey.SetImages(@"Default\Images\dk_online.png", @"Default\Images\dk_online_pressed.png");
                     break;
                 case EPersonaState.k_EPersonaStateBusy:
-                    Console.WriteLine("Resetting busy key");
                     _busyKey.SetImages(@"Default\Images\dk_busy.png", @"Default\Images\dk_busy_pressed.png");
                     break;
                 case EPersonaState.k_EPersonaStateAway:
                 case EPersonaState.k_EPersonaStateSnooze:
-                    Console.WriteLine("Resetting away key");
                     _awayKey.SetImages(@"Default\Images\dk_away.png", @"Default\Images\dk_away_pressed.png");
                     break;
                 case EPersonaState.k_EPersonaStateOffline:
-                    Console.WriteLine("Resetting offline key");
                     _offlineKey.SetImages(@"Default\Images\dk_offline.png", @"Default\Images\dk_offline_pressed.png");
                     break;
             }
@@ -134,10 +122,7 @@ namespace Sharparam.SwitchBladeSteam
         private void UpdateKeys(EPersonaState state, bool ignoreState = false)
         {
             if (!_dispatcher.CheckAccess())
-            {
-                Console.WriteLine("UpdateKeys called from thread that is not main, invoking via dispatcher...");
                 _dispatcher.Invoke(DispatcherPriority.Render, (VoidDelegate) (() => UpdateKeys(state, ignoreState)));
-            }
 
             // _state is old state, state is new state
 
@@ -155,20 +140,16 @@ namespace Sharparam.SwitchBladeSteam
                 case EPersonaState.k_EPersonaStateMax:
                 case EPersonaState.k_EPersonaStateLookingToPlay:
                 case EPersonaState.k_EPersonaStateLookingToTrade:
-                    Console.WriteLine("Activating online key");
                     _onlineKey.SetImage(@"Default\Images\dk_online_active.png");
                     break;
                 case EPersonaState.k_EPersonaStateBusy:
-                    Console.WriteLine("Activating busy key");
                     _busyKey.SetImage(@"Default\Images\dk_busy_active.png");
                     break;
                 case EPersonaState.k_EPersonaStateAway:
                 case EPersonaState.k_EPersonaStateSnooze:
-                    Console.WriteLine("Activating away key");
                     _awayKey.SetImage(@"Default\Images\dk_away_active.png");
                     break;
                 case EPersonaState.k_EPersonaStateOffline:
-                    Console.WriteLine("Activating offline key");
                     _offlineKey.SetImage(@"Default\Images\dk_offline_active.png");
                     break;
             }
